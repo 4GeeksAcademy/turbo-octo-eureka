00d0fd44cb5103dd70053f75b12a5f7c4e964d32
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2023-08-19 23:52:27 +0000

    :zap: first part of a BJ game

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,20 +2,71 @@ import React from "react";
 import PokerCard from "./pokerCard.jsx"
 
 //create your first component
-const Home = ({ title, name }) => {
+const Home = ({ title }) => {
+	
 	// Seccion logica
-	const numerito = 7;
+	const generateCard = () => {
+		const pintas = ['♥', '♦', '♣', '♠']
+		const simbols = ['Q', 'J', 'K', 'A', '2', '3', '4', '5', '6', '7', '8', '9']
+		let pinta = pintas[parseInt(Math.random() * pintas.length)]
+		let simbol = simbols[parseInt(Math.random() * simbols.length)]
+		let colorPinta = pinta == '♥' || pinta == '♦' ? 'red' : 'black'
+		return {
+			pinta,
+			simbol,
+			colorPinta
+		}	
+	}
+
+	const casa = Array(2).fill().map( x => generateCard() );
+	let mano = Array(2).fill().map( x => generateCard() );
+
+	const sumaBlack = (simbol) => {
+		if( simbol == 'Q' || simbol == 'K' || simbol == 'J') return 10;
+		if(simbol == 'A') return 11
+		return parseInt(simbol);
+	}
+
+
+	const sumaJack = (counter , value ) => counter + ( counter > 20 && value.simbol == 'A' ? 1 : sumaBlack(value.simbol) )
+
+	let total = mano.reduce(sumaJack, 0)
+
+	const totalCasa = casa.reduce(sumaJack, 0)
+
+	const addCard = () => {
+		mano = [ ...mano, generateCard() ]
+		total = mano.reduce(sumaJack, 0)
+	}
 
 	// Parte Visual
 	return (
 		<div className="text-center">
 			<h1 className="text-center mt-5">{ title }</h1>
-			<div className="d-flex flex-wrap p-1 justify-content-center align-items-center">
+			<h1>
+				{"House hand\n"}
+				({ totalCasa })
+			</h1>
+			<div className="d-flex flex-wrap p-1 my-1 justify-content-center align-items-center">
 				{ 
-					// Array(137).fill().map( x => <PokerCard /> )
-					[ <PokerCard /> , <PokerCard /> , <PokerCard />]
+					casa.map( (carta, key) => <PokerCard key={carta.simbol + key} pinta={carta.pinta} colorPinta={carta.colorPinta} simbol={carta.simbol} />)
 				}
 			</div>
+			<h1>
+				{"Player's hand\n"}
+				({ total })
+			</h1>
+			<div className="d-flex flex-wrap p-1 my-1 justify-content-center align-items-center">
+				{ 
+					mano.map( (carta, key) => <PokerCard key={carta.simbol + key} pinta={carta.pinta} colorPinta={carta.colorPinta} simbol={carta.simbol} />)
+				}
+			</div>
+			<button className="btn btn-danger mx-1">
+				Stay
+			</button>
+			<button className="btn btn-primary mx-1" onClick={addCard}>
+				Ask
+			</button>
 		</div>
 	);
 };
--- src/js/component/pokerCard.jsx
+++ src/js/component/pokerCard.jsx
@@ -1,25 +1,33 @@
 import React from "react"
 
 
-const PokerCard = () => {
+const PokerCard = ({ pinta, simbol, colorPinta}) => {
 
-    const cardStyles = { width: '18rem', color: 'black' }
-
-    const pintas = ['♥','♦','♣','♠']
-    const simbols = ['Q','J','K','A', '2', '3', '4', '5', '6', '7', '8', '9']
-
-    let pinta = pintas[parseInt(Math.random()*pintas.length)]
-    let simbol = simbols[parseInt(Math.random()*simbols.length)]
-    let colorPinta = pinta == '♥' || pinta == '♦' ? 'red' : 'black'
-
-    return (<div className="card m-2" style={cardStyles}>
-                <div className="card-body">
-                    <p style={{ color: colorPinta  }} >{pinta}</p>
-                    <h1>{simbol}</h1>
-                    <p style={{ transform: "rotate(180deg)", color: colorPinta }} >{pinta}</p>
-                </div> 
+    return (<div className="col-2 px-2">
+        <div className="card p-0">
+            <div className="card-body">
+                <p style={{
+                    color: colorPinta,
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyItems: 'end'
+                }} >
+                    {pinta}
+                </p>
+                <h1 className="">{simbol}</h1>
+                <p style={{
+                    transform: "rotate(180deg)",
+                    color: colorPinta,
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyItems: 'end'
+                }} >
+                    {pinta}
+                </p>
             </div>
-        )
+        </div>
+    </div>
+    )
 }
 
 export default PokerCard;
